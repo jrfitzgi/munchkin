d71961a0e9d8bffcbcdef7c771998b5a9acbc95e
Jordan Fitzgibbon <jordanfitzgibbon@hotmail.com>
2012-09-23 15:10:00 -0700

    initial munchkin project

---
+++ munchkin/Munchkin/Munchkin/Controllers/HomeController.vb
@@ -0,0 +1,21 @@
+﻿Public Class HomeController
+    Inherits System.Web.Mvc.Controller
+
+    Function Index() As ActionResult
+        ViewData("Message") = "MUNCH MUNCH MUNCH"
+
+        Return View()
+    End Function
+
+    Function About() As ActionResult
+        ViewData("Message") = "Your app description page."
+
+        Return View()
+    End Function
+
+    Function Contact() As ActionResult
+        ViewData("Message") = "Your contact page."
+
+        Return View()
+    End Function
+End Class
