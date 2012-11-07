aef9a786c061c6473195a2fd5924b0658e82e1e4
Jordan Fitzgibbon <jordanfitzgibbon@hotmail.com>
2012-11-06 19:10:09 -0800

    putting project back

---
+++ Munchkin/Munchkin/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Munchkin.Models;
+
+namespace Munchkin.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "C# Munchkin 2";
+
+            return View();
+        }
+
+        public ActionResult MapMe()
+        {
+            ViewBag.Message = "Maps your co-ordinates";
+            MapMeModel mmm = new MapMeModel();
+            return View(mmm);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult MapMe(MapMeModel model)
+        {
+            if (ModelState.IsValid && !String.IsNullOrWhiteSpace(model.Coordinates))
+            {
+                return View(model);
+            }
+
+            return MapMe();
+        }
+
+        public ActionResult MyLocation()
+        {
+            ViewBag.Message = "My Location";
+
+            return View(new MapMeModel());
+        }
+    }
+}
