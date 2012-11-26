0e69e11b9baf8ba4f976b693a3e5643c63d07779
Jordan Fitzgibbon <jordanfitzgibbon@hotmail.com>
2012-11-26 10:23:28 -0800

    fb image and name

--- Munchkin/Munchkin/Filters/InitializeSimpleMembershipAttribute.cs
+++ Munchkin/Munchkin/Filters/InitializeSimpleMembershipAttribute.cs
@@ -38,7 +38,7 @@ namespace Munchkin.Filters
                         }
                     }
 
-                    WebSecurity.InitializeDatabaseConnection("DefaultConnection", "UserProfile", "UserId", "UserName", autoCreateTables: true);
+                    WebSecurity.InitializeDatabaseConnection("MunchkinDB", "UserProfile", "UserId", "UserName", autoCreateTables: true);
                 }
                 catch (Exception ex)
                 {
